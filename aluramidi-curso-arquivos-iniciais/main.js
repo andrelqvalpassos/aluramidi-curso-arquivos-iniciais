f0d120f90885e1070411bdd0d60f58b1c11661d6
Andre <105212500+andrelqvalpassos@users.noreply.github.com>
2024-09-26 12:10:37 -0300

    att4

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,18 @@
 function tocaSomSax () {
-    document.querySelector( '#som_tecla_pom' ).play();
+    document.querySelector( ).play();
     
 }
 
-document.querySelector( '.tecla_pom' );
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < ListaDeTeclas.length ) {
+    ListaDeTeclas [0].onclick = tocaSomSax;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
